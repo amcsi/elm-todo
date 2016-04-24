d236a74586f46e7755722909189b141e51f95144
Attila Szeremi <attila@szeremi.org>
2016-04-24 18:25:58 +0100

    Got ability to add todos working!

--- app/Main.elm
+++ app/Main.elm
@@ -1,12 +1,14 @@
 import Html exposing (Html, div, text, input, del)
-import Html.Attributes exposing (class, type', placeholder)
-import Html.Events exposing (onKeyPress)
+import Html.Attributes exposing (class, type', placeholder, value)
+import Html.Events exposing (on, onKeyPress, targetValue)
 import List exposing (map)
 import WebAPI.Window exposing (alert)
 import Task exposing (Task)
 import Signal exposing (Signal, Address)
 import Effects exposing (Effects)
 import StartApp exposing (App)
+
+enterKey = 13
 
 app : App Model
 app =
@@ -28,18 +30,22 @@ init =
         , ("entry2", False)
         , ("entry3", False)
         ]
+      , newEntryValue = ""
       }
   in ( initModel, Effects.none)
 
 type alias TodoItem = (String, Bool)
 
 type alias Model =
-  { todos: List ((String, Bool))
+  { todos : List ((String, Bool))
+  , newEntryValue : String
   }
 
 type Action
     = NoOp
     | TodoAddFieldKeypressed Int
+    | TodoAddFieldAfterKeypressed Int
+    | UpdateField String
 
 displayTodo : TodoItem -> Html
 displayTodo (text', completed) =
@@ -63,8 +69,10 @@ view address model =
       , div []
         [ input
           [ type' "text"
+          , value model.newEntryValue
           , placeholder "Enter task description here"
-          , onKeyPress address TodoAddFieldKeypressed ]
+          , onKeyPress address TodoAddFieldKeypressed
+          , on "input" targetValue (Signal.message address << UpdateField) ]
           []
         ]
     ]
@@ -74,11 +82,18 @@ update action model =
   case action of
     TodoAddFieldKeypressed key ->
       ( case key of
-        enter ->
-          ( model
-          , alert "enter pressed" |>
-            Task.map (always NoOp) |>
-              Effects.task
-          )
+        13 ->
+          { model
+              | todos = (model.newEntryValue, False) :: model.todos
+              , newEntryValue = ""
+          }
+        _ -> model
+      , Effects.none
+      )
+    UpdateField value ->
+      ( { model
+          | newEntryValue = value
+        }
+        , Effects.none
       )
     _ -> (model, Effects.none)
